86f5d35333e98176305b93d31ba40d0ef76f5e7f
MatyasHradek <hradmat@seznam.cz>
2025-04-29 11:14:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const ctx = canvas.getContext("2d");
 const result = document.getElementById("result");
 const spinBtn = document.getElementById("spin");
 
-const segments = ["Plavba", "Výhra", "Zkus to znovu", "Prostě něco", "Výhra 2x", "Nejedeš na tábor", "Sleva 20%"];
+const segments = ["NIC!!", "Výhra", "Plavba", "Prostě něco", "Výhra 2x", "Nejedeš na tábor", "Sleva 20%"];
 const colors = ["#FF6347", "#FFD700", "#ADFF2F", "#00CED1", "#FF69B4", "#9370DB", "#32CD32"];
 const segAngle = 360 / segments.length;
 
